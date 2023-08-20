4fd20b1b340583d54dce3f09596050c3a586555e
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2023-08-20 16:05:02 -0700

    chore: remove express-tooling module

--- src/express-tooling/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./create-express-request-handler";
-export * from "./create-express-validation-handler";
-export * from "./create-handler-argument-mapper";
-export * from "./extract-express-controller-router";
